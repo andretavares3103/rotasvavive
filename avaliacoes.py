8e64ea42278a08fc5f1f425222d06494c5b35e57
andretavares3103 <andre.mtavares1@gmail.com>
2025-06-30 12:44:15 -0300

    Update avaliacoes.py

--- avaliacoes.py
+++ avaliacoes.py
@@ -751,7 +751,7 @@ def pipeline(file_path, output_dir):
         df_distancias_alerta.to_excel(writer, sheet_name="df_distancias_alert", index=False)
     return final_path
 
-tabs = st.tabs([ "Portal Atendimentos", "Upload de Arquivo", "Matriz de Rotas", "Aceites"])
+#tabs = st.tabs([ "Portal Atendimentos", "Upload de Arquivo", "Matriz de Rotas", "Aceites"])
 tab_labels = ["Portal Atendimentos", "Upload de Arquivo", "Matriz de Rotas", "Aceites"]
 tabs = st.tabs(tab_labels)
 
